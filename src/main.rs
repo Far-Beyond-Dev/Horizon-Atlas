f2bb1bf41e0f1c799d7ec007fc4c93356a0eb9ab
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2024-05-18 14:58:11 -0400

    Major Rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,117 @@
-use tokio::net::TcpListener;
-use tokio_tungstenite::tungstenite::Message;
-use tokio_tungstenite::accept_async;
-use futures_util::{StreamExt, SinkExt};
-use std::collections::HashMap;
+use serde_json::{json, Value}; // Import json macro and Value from serde_json
+use socketioxide::{
+    extract::{AckSender, Bin, Data, SocketRef},
+    SocketIo,
+};
 use std::sync::{Arc, Mutex};
-use serde::{Serialize, Deserialize};
-use log::{info, error};
-use config::{Config, ConfigError, File, Environment};
+use tracing::{debug, error, event, info, Level};
+use tracing_subscriber::FmtSubscriber;
+use std::io::Write; // Bring the Write trait into scope
+use viz::{handler::ServiceHandler, serve, Result, Router};
+use serde::Deserialize;
+use std::fs;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct InstanceInfo {
-    id: String,
-    address: String,
+// Define a struct to represent the configuration
+#[derive(Debug, Deserialize)]
+struct Config {
+    server: ServerConfig,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct GameState {
-    players: HashMap<String, String>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 struct ServerConfig {
-    host: String,
+    address: String,
     port: u16,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct DatabaseConfig {
-    #[serde(rename = "type")]
-    db_type: String,
-    connection_string: String,
+// Function to load the configuration from file
+fn load_config() -> Result<Config, Box<dyn std::error::Error>> {
+    let config_str = fs::read_to_string("config.toml")?;
+    let config: Config = toml::from_str(&config_str)?;
+    Ok(config)
+}
+// Define a struct for Player
+#[derive(Debug)]
+struct Player {
+    id: String,
+    socket: SocketRef, // Include the SocketRef to associate player with their connection
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct LoggingConfig {
-    level: String,
-}
+fn on_connect(socket: SocketRef, Data(data): Data<Value>, players: Arc<Mutex<Vec<Player>>>) {
+    // Create a new Player instance and add it to the players array
+    let player = Player {
+        id: socket.id.to_string(), // Convert Sid to String
+        socket: socket.clone(),    // Store the SocketRef
+    };
+    players.lock().unwrap().push(player);
 
-#[derive(Debug, Serialize, Deserialize)]
-struct InstancesConfig {
-    instance_1: String,
-    instance_2: String,
-}
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
 
-#[derive(Debug, Serialize, Deserialize)]
-struct AppConfig {
-    server: ServerConfig,
-    database: DatabaseConfig,
-    logging: LoggingConfig,
-    instances: InstancesConfig,
-}
+    let players_arc = Arc::clone(&players);
 
-impl AppConfig {
-    pub fn from_file(file: &str) -> Result<Self, ConfigError> {
-        let settings = Config::builder()
-            .add_source(File::with_name(file))
-            .add_source(Environment::with_prefix("APP"))
-            .build()?;
-        
-        settings.try_deserialize()
-    }
-}
+    socket.on(
+        "UpdatePlayerLocation",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            socket.bin(bin).emit("message-back", data).ok();
+        },
+    );
 
-type SharedGameState = Arc<Mutex<GameState>>;
-type SharedInstanceInfo = Arc<Mutex<HashMap<String, InstanceInfo>>>;
+    socket.on(
+        "message-with-ack",
+        |Data::<Value>(data), ack: AckSender, Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            ack.bin(bin).send(data).ok();
+        },
+    );
+
+    // Register the event handler to send online players array to the client
+    socket.on(
+        "getOnlinePlayers",
+        move |socket: SocketRef, _: Data<Value>, _: Bin| {
+            info!("Responding with online players list");
+            let players = players.lock().unwrap(); // Lock mutex to access players array
+
+            let online_players_json = serde_json::to_value(
+                players 
+                    .iter()
+                    .map(|player| json!({ "id": &player.id })) // Use json! macro to create JSON object
+                    .collect::<Vec<_>>(),
+            )
+            .unwrap(); // Serialize online players array to JSON
+
+            debug!("Player Array as JSON{}", online_players_json);
+
+            socket.emit("onlinePlayers", online_players_json).ok(); // Emit online players array to client
+        },
+    );
+
+    // Broadcast a message to all connected clients
+    socket.on("broadcastMessage", move |Data::<Value>(data), _: Bin| {
+        let players = players_arc.lock().unwrap(); // Lock mutex to access players array
+        for player in &*players {
+            player.socket.emit("broadcastMessage", data.clone()).ok(); // Access the associated SocketRef and emit the message
+        }
+    });
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Basic print statements to ensure they work before any other code
+    std::io::stdout().flush().unwrap(); // Ensure the buffer is flushed
+
     // Load configuration
-    println!("Loading configs...");
-    let config = AppConfig::from_file("config.toml")?;
-    
-    // Initialize the logger with the specified log level
-    std::env::set_var("RUST_LOG", &config.logging.level);
-    env_logger::init();
+    let config = load_config()?;
+    let address = format!("{}:{}", config.server.address, config.server.port);
 
-    println!("Initilizing Game server...");
-    let game_state: SharedGameState = Arc::new(Mutex::new(GameState {
-        players: HashMap::new(),
-    }));
+    // Initialize the tracing subscriber with INFO log level
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
 
-    let instance_info: SharedInstanceInfo = Arc::new(Mutex::new(HashMap::new()));
-
-    let addr = format!("{}:{}", config.server.host, config.server.port);
-    let listener = TcpListener::bind(&addr).await.expect("Can't bind to address");
+    println!("Starting Horizon Atlas...");
+    println!("");
 
     println!("+--------------------------------------------------------------------------------------------------------------------------------+");
     println!("|    __    __                         __                                          ______      __       __                        |");
@@ -99,44 +127,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("+--------------------------------------------------------------------------------------------------------------------------------+");
                                                                                                                 
                                                                                                                       
-    println!("Horizon Atlas is running on {}", addr);
+    println!("Horizon Atlas is running on {}", address);
+    // Initialize the players array as an Arc<Mutex<Vec<Player>>>
+    let players: Arc<Mutex<Vec<Player>>> = Arc::new(Mutex::new(Vec::new()));
 
-    while let Ok((stream, _)) = listener.accept().await {
-        let game_state = game_state.clone();
-        let instance_info = instance_info.clone();
+    let (svc, io) = socketioxide::SocketIo::new_svc();
 
-        tokio::spawn(async move {
-            let ws_stream = accept_async(stream).await.expect("Error during the websocket handshake");
-            let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+    let players_clone = players.clone();
+    io.ns("/", move |socket, data| {
+        on_connect(socket, data, players_clone.clone())
+    });
 
-            while let Some(message) = ws_receiver.next().await {
-                match message {
-                    Ok(Message::Text(text)) => {
-                        if let Ok(instance) = serde_json::from_str::<InstanceInfo>(&text) {
-                            info!("Registering instance: {:?}", instance);
-                            instance_info.lock().unwrap().insert(instance.id.clone(), instance);
-                            ws_sender.send(Message::Text("Instance registered".to_string())).await.unwrap();
-                        } else if let Ok(new_state) = serde_json::from_str::<GameState>(&text) {
-                            info!("Received new game state: {:?}", new_state);
-                            let mut state = game_state.lock().unwrap();
-                            *state = new_state;
-                        } else {
-                            error!("Received invalid message: {}", text);
-                        }
-                    },
-                    Ok(Message::Binary(_)) => error!("Unexpected binary message"),
-                    Ok(Message::Close(_)) => {
-                        info!("Connection closed");
-                        break;
-                    },
-                    Err(e) => {
-                        error!("Error receiving message: {}", e);
-                        break;
-                    },
-                    _ => {}
-                }
-            }
-        });
+    let app = Router::new()
+        .get("/", |_| async { Ok("Hello, World!") })
+        .any("/*", ServiceHandler::new(svc));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    info!("Server listening on interface 0.0.0.0 with port 3000");
+    std::io::stdout().flush().unwrap(); // Ensure the buffer is flushed
+
+    if let Err(e) = serve(listener, app).await {
+        error!("{}", e);
     }
 
     Ok(())
